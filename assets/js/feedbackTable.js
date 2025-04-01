e400f1e78d446eeba34bb1fc344d14b55b138526
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-04-01 12:30:48 +0100

    Remove unused DataTables code

--- assets/js/feedbackTable.js
+++ assets/js/feedbackTable.js
@@ -1,8 +1,3 @@
-function cleanColumnOutput(data) {
-  const unsafeOutputPattern = />|<|&|"|\/|'/g
-  return data.replace(unsafeOutputPattern, '')
-}
-
 function createTable({
   id,
   ajaxUrl,
@@ -109,99 +104,32 @@ function createTable({
   })
 }
 
-function formatMonitorName(name) {
-  return `${name} `
-    .trim()
-    .toLowerCase()
-    .replace(/ /g, '-')
-    .replace(/[^-a-z0-9]/g, '')
-    .replace(/-+/g, '-')
-}
-
 jQuery(function () {
   const columns = [
     {
       data: 'title',
-      // createdCell: function (td, _cellData, rowData) {
-      //   $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
-      // },
     },
     {
       data: 'establistment',
-      // createdCell: function (td, _cellData, rowData) {
-      //   $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
-      // },
     },
     {
       data: 'contentType',
-      // createdCell: function (td, _cellData, rowData) {
-      //   $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
-      // },
     },
     {
       data: 'sentiment',
-      // createdCell: function (td, _cellData, rowData) {
-      //   $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
-      // },
     },
     {
       data: 'comment',
-      // createdCell: function (td, _cellData, rowData) {
-      //   $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
-      // },
     },
     {
       data: 'date',
-      // createdCell: function (td, _cellData, rowData) {
-      //   $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
-      // },
     },
     {
       data: 'categories',
-      // createdCell: function (td, _cellData, rowData) {
-      //   $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
-      // },
     },
     {
       data: 'series',
-      // createdCell: function (td, _cellData, rowData) {
-      //   $(td).html(`<a href="/teams/${rowData.attributes.slug}">${rowData.attributes.name}</a>`)
-      // },
     },
-    // {
-    //   data: 'attributes.slack_channel_name',
-    //   createdCell: function (td, _cellData, rowData) {
-    //     if (rowData.attributes.slack_channel_id)
-    //       $(td).html(
-    //         `<a href="slack://channel?team=T02DYEB3A&id=${rowData.attributes.slack_channel_id}">#${rowData.attributes.slack_channel_name}</a>`,
-    //       )
-    //   },
-    // },
-    // {
-    //   data: 'attributes.products',
-    //   createdCell: function (td, _cellData, rowData) {
-    //     const products = rowData.attributes.products.data
-    //       .map(
-    //         product =>
-    //           `<li><a href="/products/${product.attributes.slug}" data-test="product-${product.id}">${product.attributes.name}</a></li>`,
-    //       )
-    //       .join('')
-    //     if (products) {
-    //       $(td).html(`${products}`)
-    //     } else {
-    //       $(td).html(`No Products`)
-    //     }
-    //   },
-    // },
-    // {
-    //   data: 'attributes.slug',
-    //   createdCell: function (td, _cellData, rowData) {
-    //     const monitor_name = `${formatMonitorName(rowData.attributes.name)}`
-    //     $(td).html(
-    //       `<details class="govuk-details"><summary class="govuk-details__summary"><span class="govuk-details__summary-text">Links</span></summary><li><a class="govuk-link--no-visited-state" href="/monitor/team/${monitor_name}">Health Monitor</a></li><li><a class="govuk-link--no-visited-state" href="/drift-radiator/teams/${monitor_name}">Deployment drift</a></li><li><a class="govuk-link--no-visited-state" href="/trivy/teams/${monitor_name}">Trivy</a></li>`,
-    //     )
-    //   },
-    // },
   ]
 
   createTable({ id: 'feedbackTable', ajaxUrl: '/reports/data', orderColumn: 4, orderType: 'desc', columns })
