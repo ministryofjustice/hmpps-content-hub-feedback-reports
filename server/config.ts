bd651db43e6f574ec43aed294cb94a5f75a266ac
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2025-10-10 11:09:55 +0100

    reverted additional config

--- server/config.ts
+++ server/config.ts
@@ -110,7 +110,6 @@ export default {
       ssl: {
         rejectUnauthorized: false,
         cert: readFileSync(path.join(__dirname, '../../global-bundle.pem')),
-        sslMode: get('FEEDBACK_DATABASE_SSL_MODE', 'prefer'),
       },
     },
   },
