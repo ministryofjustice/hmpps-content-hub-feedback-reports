2fb0ad20bd21f7cfd000b03b34efb9e1fccf1bf6
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2025-10-23 08:52:39 +0100

    fix tests

--- integration_tests/mockApis/auth.ts
+++ integration_tests/mockApis/auth.ts
@@ -130,6 +130,8 @@ export default {
   getSignInUrl,
   stubAuthPing: ping,
   stubAuthManageDetails: manageDetails,
-  stubSignIn: (userToken: UserToken = {}): Promise<[Response, Response, Response, Response, Response]> =>
+  stubSignIn: (
+    userToken: UserToken = { roles: ['ROLE_CONTENT_HUB_FEEDBACK_USER'] },
+  ): Promise<[Response, Response, Response, Response, Response]> =>
     Promise.all([favicon(), redirect(), signOut(), token(userToken), tokenVerification.stubVerifyToken()]),
 }
