4cd5fffd3d18a0d456bc82e85d825b8d5076ba02
Angela Pan <angela.pan@digital.justice.gov.uk>
2025-10-24 11:52:15 +0100

    Update pipelines and ready for prod release (#27)
    * updated pipeline and set auditing to off
    
    * add auth group to restrict access
    
    * reverted additional config
    
    * updated env names to match cloud platform
    
    * fix tests
    
    * fix tests
    
    * fix spacing

--- server/app.ts
+++ server/app.ts
@@ -35,7 +35,7 @@ export default function createApp(services: Services): express.Application {
   app.use(setUpStaticResources())
   nunjucksSetup(app)
   app.use(setUpAuthentication())
-  app.use(authorisationMiddleware())
+  app.use(authorisationMiddleware(['CONTENT_HUB_FEEDBACK_USER']))
   app.use(setUpCsrf())
   app.use(setUpCurrentUser())
 
